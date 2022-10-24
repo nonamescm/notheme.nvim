aa908b89b267c61008ac55d8b4109c1599e2ff82
nonamescm <snadragona12bionic@gmail.com>
2022-10-24 13:24:23 -0300

    fix the unlinked highlights

--- lua/notheme/init.lua
+++ lua/notheme/init.lua
@@ -11,176 +11,261 @@ local vim = vim
 local function hi(group, styles)
 	local cmd = vim.cmd or vim.command
 
-	local gui = styles.gui and 'gui='..styles.gui or 'gui=NONE'
-	local sp = styles.sp and 'guisp='..styles.sp or 'guisp=NONE'
-	local fg = styles.fg and 'guifg='..styles.fg or 'guifg=NONE'
-	local bg = styles.bg and 'guibg='..styles.bg or 'guibg=NONE'
-	cmd('highlight '..group..' '..gui..' '..sp..' '..fg..' '..bg)
+	if styles.link then
+		cmd("highlight link " .. group .. " " .. styles.link)
+	else
+		local gui = styles.gui and "gui="..styles.gui or "gui=NONE"
+		local sp = styles.sp and "guisp="..styles.sp or "guisp=NONE"
+		local fg = styles.fg and "guifg="..styles.fg or "guifg=NONE"
+		local bg = styles.bg and "guibg="..styles.bg or "guibg=NONE"
+		cmd("highlight "..group.." "..gui.." "..sp.." "..fg.." "..bg)
+	end
 end
 
-local bg_normal   = '#282838'
-local bg_darker   = '#242434'
-local bg_dark     = '#2b2b3b'
-local bg          = '#393d4e'
-local bg_light    = '#42475d'
-local bg_lighter  = '#545277'
-local grey        = '#97a6c0'
-local red         = '#f07178'
-local heavy_red   = '#ff5370'
-local green       = '#7cd380'
-local blue        = '#729aef'
-local yellow      = '#ffcb6b'
-local orange      = '#e37858'
-local purple      = '#b782da'
-local cyan        = '#529dbc'
-local fg          = '#bbc2cf'
-local fg_light    = '#b6bcdd'
-local fg_dark     = '#6d749c'
-local vis         = '#424760'
-local vis_light   = '#30354e'
-local white       = '#ffffff'
+local bg_normal   = "#282838"
+local bg_darker   = "#242434"
+local bg_dark     = "#2b2b3b"
+local bg          = "#393d4e"
+local bg_light    = "#42475d"
+local bg_lighter  = "#545277"
+local grey        = "#97a6c0"
+local red         = "#f07178"
+local heavy_red   = "#ff5370"
+local green       = "#7cd380"
+local blue        = "#729aef"
+local yellow      = "#ffcb6b"
+local orange      = "#e37858"
+local purple      = "#b782da"
+local cyan        = "#529dbc"
+local fg          = "#bbc2cf"
+local fg_light    = "#b6bcdd"
+local fg_dark     = "#6d749c"
+local vis         = "#424760"
+local vis_light   = "#30354e"
+local white       = "#ffffff"
 
 -- }}}
 
 -- Editor Highlight Groups {{{
 
-hi('CursorLine'  , { bg = "NONE" })
-hi('Cursor'      , { fg = bg_dark, bg = yellow })
-hi('Directory'   , { fg = blue, gui = 'bold' })
-hi('DiffAdd'     , { fg = green })
-hi('DiffChange'  , { fg = yellow })
-hi('DiffDelete'  , { fg = red })
-hi('DiffText'    , { fg = blue })
-hi('EndOfBuffer' , { bg = bg_normal, fg = bg_normal })
-hi('ErrorMsg'    , { fg = red, gui = 'bold' })
-hi('VertSplit'   , { bg = "NONE", fg = bg_normal })
-hi('Folded'      , { fg = fg_dark, gui = 'italic' })
-hi('FoldColumn'  , { fg = yellow })
-hi('SignColumn'  , { fg = yellow })
-hi('IncSearch'   , { bg = yellow, fg = bg })
-hi('Substitute'  , { bg = blue, fg = bg })
-hi('LineNr'      , { fg = vis })
-hi('CursorLineNr', { fg = fg_dark })
-hi('MatchParen'  , { fg = "NONE", bg = bg_light })
-hi('Normal'      , { fg = fg_light, bg = bg_normal })
+hi("CursorLine"  , { bg = "NONE" })
+hi("Cursor"      , { fg = bg_dark, bg = yellow })
+hi("Directory"   , { fg = blue, gui = "bold" })
+hi("DiffAdd"     , { fg = green })
+hi("DiffChange"  , { fg = yellow })
+hi("DiffDelete"  , { fg = red })
+hi("DiffText"    , { fg = blue })
+hi("EndOfBuffer" , { bg = bg_normal, fg = bg_normal })
+hi("ErrorMsg"    , { fg = red, gui = "bold" })
+hi("VertSplit"   , { bg = "NONE", fg = bg_darker })
+hi("Folded"      , { fg = fg_dark, gui = "italic" })
+hi("FoldColumn"  , { fg = yellow })
+hi("SignColumn"  , { fg = yellow })
+hi("IncSearch"   , { bg = yellow, fg = bg })
+hi("Substitute"  , { bg = blue, fg = bg })
+hi("LineNr"      , { fg = vis })
+hi("CursorLineNr", { fg = fg_dark })
+hi("MatchParen"  , { fg = "NONE", bg = bg_light })
+hi("Normal"      , { fg = fg_light, bg = bg_normal })
 
-hi('NormalFloat' , { bg = bg_darker })
-hi('FloatBorder' , { fg = vis, bg = bg_darker })
+hi("NormalFloat" , { bg = bg_darker })
+hi("FloatBorder" , { fg = vis, bg = bg_darker })
 
-hi('Pmenu'       , { bg = bg_darker, fg = fg_light })
-hi('PmenuSel'    , { bg = bg, fg = fg_light })
-hi('PmenuSbar'   , { bg = bg_darker })
-hi('PmenuThumb'  , { bg = vis_light })
-hi('Search'      , { bg = vis })
-hi('SpellBad'    , { gui = 'underline', sp = red })
-hi('SpellCap'    , { gui = 'underline', sp = yellow })
-hi('SpellLocal'  , { gui = 'underline', sp = orange })
-hi('SpellRare'   , { gui = 'underline', sp = blue })
-hi('TabLine'     , { bg = bg, fg = fg_light })
-hi('TabLineFill' , { bg = bg, fg = fg_light })
-hi('TabLineSel'  , { bg = cyan, fg = bg_dark })
-hi('Title'       , { fg = green })
-hi('Visual'      , { bg = vis_light })
-hi('VisualNOS'   , { bg = vis_light })
-hi('WarningMsg'  , { fg = yellow, gui = 'italic' })
-hi('Whitespace'  , { fg = fg_dark })
+hi("Pmenu"       , { bg = bg_darker, fg = fg_light })
+hi("PmenuSel"    , { bg = bg, fg = fg_light })
+hi("PmenuSbar"   , { bg = bg_darker })
+hi("PmenuThumb"  , { bg = vis_light })
+hi("Search"      , { bg = vis })
+hi("SpellBad"    , { gui = "underline", sp = red })
+hi("SpellCap"    , { gui = "underline", sp = yellow })
+hi("SpellLocal"  , { gui = "underline", sp = orange })
+hi("SpellRare"   , { gui = "underline", sp = blue })
+hi("TabLine"     , { bg = bg, fg = fg_light })
+hi("TabLineFill" , { bg = bg, fg = fg_light })
+hi("TabLineSel"  , { bg = cyan, fg = bg_dark })
+hi("Title"       , { fg = green })
+hi("Visual"      , { bg = vis_light })
+hi("VisualNOS"   , { bg = vis_light })
+hi("WarningMsg"  , { fg = yellow })
+hi("Whitespace"  , { fg = fg_dark })
 
 -- git highlighting
-hi('gitcommitComment'        , { fg = fg_dark })
-hi('gitcommitUntracked'      , { fg = fg_dark, gui = 'italic' })
-hi('gitcommitDiscarded'      , { fg = fg_dark, gui = 'italic' })
-hi('gitcommitSelected'       , { fg = fg_dark, gui = 'italic' })
-hi('gitcommitUnmerged'       , { fg = green })
-hi('gitcommitBranch'         , { fg = purple })
-hi('gitcommitNoBranch'       , { fg = purple })
-hi('gitcommitDiscardedType'  , { fg = red })
-hi('gitcommitSelectedType'   , { fg = green })
-hi('gitcommitUntrackedFile'  , { fg = cyan })
-hi('gitcommitDiscardedFile'  , { fg = red })
-hi('gitcommitDiscardedArrow' , { fg = red })
-hi('gitcommitSelectedFile'   , { fg = green })
-hi('gitcommitSelectedArrow'  , { fg = green })
-hi('gitcommitUnmergedFile'   , { fg = yellow })
-hi('gitcommitUnmergedArrow'  , { fg = yellow })
-hi('gitcommitSummary'        , { fg = fg_light })
-hi('gitcommitOverflow'       , { fg = red })
-hi('gitcommitOnBranch'       , {})
-hi('gitcommitHeader'         , {})
-hi('gitcommitFile'           , {})
+hi("gitcommitComment"        , { fg = fg_dark })
+hi("gitcommitUntracked"      , { fg = fg_dark, gui = "italic" })
+hi("gitcommitDiscarded"      , { fg = fg_dark, gui = "italic" })
+hi("gitcommitSelected"       , { fg = fg_dark, gui = "italic" })
+hi("gitcommitUnmerged"       , { fg = green })
+hi("gitcommitBranch"         , { fg = purple })
+hi("gitcommitNoBranch"       , { fg = purple })
+hi("gitcommitDiscardedType"  , { fg = red })
+hi("gitcommitSelectedType"   , { fg = green })
+hi("gitcommitUntrackedFile"  , { fg = cyan })
+hi("gitcommitDiscardedFile"  , { fg = red })
+hi("gitcommitDiscardedArrow" , { fg = red })
+hi("gitcommitSelectedFile"   , { fg = green })
+hi("gitcommitSelectedArrow"  , { fg = green })
+hi("gitcommitUnmergedFile"   , { fg = yellow })
+hi("gitcommitUnmergedArrow"  , { fg = yellow })
+hi("gitcommitSummary"        , { fg = fg_light })
+hi("gitcommitOverflow"       , { fg = red })
+hi("gitcommitOnBranch"       , {})
+hi("gitcommitHeader"         , {})
+hi("gitcommitFile"           , {})
 
-hi('Conceal'     , {})
-hi('ModeMsg'     , {})
-hi('MsgArea'     , {})
-hi('MsgSeparator', {})
-hi('MoreMsg'     , {})
-hi('NonText'     , {})
-hi('Question'    , {})
-hi('QuickFixLine', {})
-hi('StatusLine'  , {})
-hi('StatusLineNC', { gui = "NONE", bg = "NONE", fg = "NONE" })
-hi('WildMenu'    , {})
+hi("Conceal"     , {})
+hi("ModeMsg"     , {})
+hi("MsgArea"     , {})
+hi("MsgSeparator", {})
+hi("MoreMsg"     , {})
+hi("NonText"     , {})
+hi("Question"    , {})
+hi("QuickFixLine", {})
+hi("StatusLine"  , {})
+hi("StatusLineNC", { gui = "NONE", bg = "NONE", fg = "NONE" })
+hi("WildMenu"    , {})
 
 -- }}}
 
 -- Vim Default Code Syntax {{{
 
-hi('Comment'       , { fg = fg_dark })
-hi('Constant'      , { fg = yellow })
-hi('String'        , { fg = green })
-hi('Character'     , { fg = green })
-hi('Number'        , { fg = orange })
-hi('Float'         , { fg = orange })
-hi('Boolean'       , { fg = orange })
+hi("Comment"       , { fg = fg_dark })
+hi("Constant"      , { fg = yellow })
+hi("String"        , { fg = green })
+hi("Character"     , { fg = green })
+hi("Number"        , { fg = orange })
+hi("Float"         , { fg = orange })
+hi("Boolean"       , { fg = orange })
 
-hi('Identifier'    , { fg = heavy_red })
-hi('Function'      , { fg = cyan })
+hi("Identifier"    , { fg = heavy_red })
+hi("Function"      , { fg = cyan })
 
-hi('Statement'     , { fg = blue })
-hi('Conditional'   , { fg = purple })
-hi('Repeat'        , { fg = purple })
-hi('Label'         , { fg = cyan })
-hi('Exception'     , { fg = cyan })
-hi('Operator'      , { fg = purple })
-hi('Keyword'       , { fg = purple })
+hi("Statement"     , { fg = blue })
+hi("Conditional"   , { fg = purple })
+hi("Repeat"        , { fg = purple })
+hi("Label"         , { fg = cyan })
+hi("Exception"     , { fg = cyan })
+hi("Operator"      , { fg = purple })
+hi("Keyword"       , { fg = purple })
 
-hi('Include'       , { fg = blue })
-hi('Define'        , { fg = purple })
-hi('Macro'         , { fg = purple })
-hi('PreProc'       , { fg = yellow })
-hi('PreCondit'     , { fg = yellow })
+hi("Include"       , { fg = blue })
+hi("Define"        , { fg = purple })
+hi("Macro"         , { fg = purple })
+hi("PreProc"       , { fg = yellow })
+hi("PreCondit"     , { fg = yellow })
 
-hi('Type'          , { fg = yellow })
-hi('StorageClass'  , { fg = purple })
-hi('Structure'     , { fg = purple })
-hi('Typedef'       , { fg = purple })
+hi("Type"          , { fg = yellow })
+hi("StorageClass"  , { fg = purple })
+hi("Structure"     , { fg = purple })
+hi("Typedef"       , { fg = purple })
 
-hi('Special'       , { })
-hi('SpecialChar'   , { })
-hi('SpecialKey'    , {  fg = fg_light })
-hi('Tag'           , { fg = orange })
-hi('SpecialComment', { fg = fg_dark, gui = 'bold' })
-hi('Debug'         , {})
-hi('Delimiter'     , {})
+hi("Special"       , { })
+hi("SpecialChar"   , { })
+hi("SpecialKey"    , {  fg = fg_light })
+hi("Tag"           , { fg = orange })
+hi("SpecialComment", { fg = fg_dark, gui = "bold" })
+hi("Debug"         , {})
+hi("Delimiter"     , {})
 
-hi('Ignore'        , {})
-hi('Underlined'    , { gui = 'underline' })
-hi('Error'         , { fg = heavy_red })
-hi('Todo'          , { fg = purple, gui = 'bold' })
+hi("Ignore"        , {})
+hi("Underlined"    , { gui = "underline" })
+hi("Error"         , { fg = heavy_red })
+hi("Todo"          , { fg = purple, gui = "bold" })
 
 -- }}}
 
 -- Lsp Highlight {{{
-hi('DiagnosticError'      , { fg = red })
-hi('DiagnosticWarn'       , { fg = yellow })
-hi('DiagnosticHint'       , { fg = green })
-hi('DiagnosticMessage'    , { fg = fg })
+hi("DiagnosticError"      , { fg = red })
+hi("DiagnosticWarn"       , { fg = yellow })
+hi("DiagnosticHint"       , { fg = green })
+hi("DiagnosticMessage"    , { fg = fg })
 -- }}}
 
 -- tree-sitter highlight {{{
 
+hi("TSPunctDelimiter", { link = "Delimiter" })
+hi("TSPunctBracket", { link = "Delimiter" })
+hi("TSPunctSpecial", { link = "Delimiter" })
+
+hi("TSConstant", { link = "Constant" })
+hi("TSConstBuiltin", { link = "Special" })
+hi("TSConstMacro", { link = "Define" })
+hi("TSString", { link = "String" })
+hi("TSStringRegex", { link = "String" })
+hi("TSStringEscape", { link = "SpecialChar" })
+hi("TSStringSpecial", { link = "SpecialChar" })
+hi("TSCharacter", { link = "Character" })
+hi("TSCharacterSpecial", { link = "SpecialChar" })
+hi("TSNumber", { link = "Number" })
+hi("TSBoolean", { link = "Boolean" })
+hi("TSFloat", { link = "Float" })
+
+hi("TSFunction", { link = "Function" })
+hi("TSFunctionCall", { link = "TSFunction" })
+hi("TSFuncBuiltin", { link = "Special" })
+hi("TSFuncMacro", { link = "Macro" })
+hi("TSParameter", { link = "Identifier" })
+hi("TSParameterReference", { link = "TSParameter" })
+hi("TSMethod", { link = "Function" })
+hi("TSMethodCall", { link = "TSMethod" })
+hi("TSField", { link = "Identifier" })
+hi("TSProperty", { link = "Identifier" })
+hi("TSConstructor", { link = "Special" })
+hi("TSAnnotation", { link = "PreProc" })
+hi("TSAttribute", { link = "PreProc" })
+hi("TSNamespace", { link = "Include" })
+hi("TSSymbol", { link = "Identifier" })
+
+hi("TSConditional", { link = "Conditional" })
+hi("TSRepeat", { link = "Repeat" })
+hi("TSLabel", { link = "Label" })
+hi("TSOperator", { link = "Operator" })
+hi("TSKeyword", { link = "Keyword" })
+hi("TSKeywordFunction", { link = "Keyword" })
+hi("TSKeywordOperator", { link = "TSOperator" })
+hi("TSKeywordReturn", { link = "TSKeyword" })
+hi("TSException", { link = "Exception" })
+hi("TSDebug", { link = "Debug" })
+hi("TSDefine", { link = "Define" })
+hi("TSPreProc", { link = "PreProc" })
+hi("TSStorageClass", { link = "StorageClass" })
+
+hi("TSTodo", { link = "Todo" })
+
+hi("TSType", { link = "Type" })
+hi("TSTypeBuiltin", { link = "Type" })
+hi("TSTypeQualifier", { link = "Type" })
+hi("TSTypeDefinition", { link = "Typedef" })
+
+hi("TSInclude", { link = "Include" })
+
+hi("TSVariableBuiltin", { link = "Special" })
+
+hi("TSText", { link = "TSNone" })
+hi("TSStrong", {})
+hi("TSEmphasis", {})
+hi("TSUnderline", {})
+hi("TSStrike", {})
+
+hi("TSMath", { link = "Special" })
+hi("TSTextReference", { link = "Constant" })
+hi("TSEnvironment", { link = "Macro" })
+hi("TSEnvironmentName", { link = "Type" })
+hi("TSTitle", { link = "Title" })
+hi("TSLiteral", { link = "String" })
+hi("TSURI", { link = "Underlined" })
+
+hi("TSComment", { link = "Comment" })
+hi("TSNote", { link = "SpecialComment" })
+hi("TSWarning", { link = "Todo" })
+hi("TSDanger", { link = "WarningMsg" })
+
+hi("TSTag", { link = "Label" })
+hi("TSTagDelimiter", { link = "Delimiter" })
+hi("TSTagAttribute", { link = "TSProperty" })
+
 hi("TSAnnotation",  { fg = red })
 hi("TSBoolean", { fg = orange })
-hi("TSCharacter", { fg = green, gui = 'bold' })
+hi("TSCharacter", { fg = green, gui = "bold" })
 hi("TSConstBuiltin", { fg = blue })
 hi("TSConstMacro", { fg = blue })
 hi("TSConstructor", { fg = yellow })
